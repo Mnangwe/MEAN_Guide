ecd35b6314070d88f42c55505b0a7bba916c37cc
Azabenathi Pupuma <pupuma.azabenathi@gmail.com>
2023-01-10 10:49:56 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularSecComponent } from './components/angular-sec/angular-sec.component';
+import { DatabaseComponent } from './components/database/database.component';
+import { DeploymentSecComponent } from './components/deployment-sec/deployment-sec.component';
+import { HomeComponent } from './components/home/home.component';
+import { NodeSecComponent } from './components/node-sec/node-sec.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'angular', component: AngularSecComponent
+  },
+  {
+    path: 'node', component: NodeSecComponent
+  },
+  {
+    path: 'mongo-db', component: DatabaseComponent
+  },
+  {
+    path: 'deploy', component: DeploymentSecComponent
+  },
+  {
+    path: 'error-not-found', component: NotFoundPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
